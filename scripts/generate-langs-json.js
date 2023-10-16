8afc0e399344f89c5cc58848a9d64dfbfc2f2ed8
Lingyun Zhang <100548907+zly5@users.noreply.github.com>
2023-10-16 21:12:10 +0800

    Update generate-langs-json.js

--- scripts/generate-langs-json.js
+++ scripts/generate-langs-json.js
@@ -5,3 +5,27 @@ import jsYaml from "js-yaml";
 const LANGS_FILEPATH = "./src/common/languageColors.json";
 
 
+//Retrieve languages from github linguist repository yaml file
+//@ts-ignore
+axios
+  .get(
+    "./src/common/languageColors.json",
+  )
+  .then((response) => {
+    //and convert them to a JS Object
+    const languages = jsYaml.load(response.data);
+
+    const languageColors = {};
+
+    //Filter only language colors from the whole file
+    Object.keys(languages).forEach((lang) => {
+      languageColors[lang] = languages[lang].color;
+    });
+
+    //Debug Print
+    //console.dir(languageColors);
+    fs.writeFileSync(
+      LANGS_FILEPATH,
+      JSON.stringify(languageColors, null, "    "),
+    );
+  });
